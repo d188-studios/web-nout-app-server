cdeac38e1d94b1a05f25fbd7f45f1347d87abe8d
dobresoft <42759240+dobresoft@users.noreply.github.com>
2022-06-01 11:49:39 -0500

    Borre un comentario

--- src/api/middlewares/validateContent.js
+++ src/api/middlewares/validateContent.js
@@ -2,8 +2,6 @@ const validationErrors = require("./validationErrors");
 const { body } = require("express-validator");
 
 module.exports = [
-  //body("content").not().isEmpty().withMessage("Content can't be empty"),
-
   body("content").isLength({ min: 0 }).withMessage("Content can't be empty"),
 
   body("content").isArray().withMessage("Content must be an array"),
